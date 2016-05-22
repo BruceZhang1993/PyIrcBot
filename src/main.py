e899b76bd6a99095c77b4649327793a6fcbeacb3
Bruce Zhang <zttt183525594@gmail.com>
2016-05-22 19:51:41 +0800

    packaging

--- src/main.py
+++ src/main.py
@@ -23,8 +23,8 @@ PLUGINDIR = './plugins/'
 sys.path.append(PLUGINDIR)
 
 
-from .plugins.echo import echo
-from .plugins.linkhandler import linkhandler
+from plugins.echo import echo
+from plugins.linkhandler import linkhandler
 pluginlist = ["echo"]
 handlerlist = ["linkhandler"]
 funclist = [echo]
