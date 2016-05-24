5a7346aacb2577b69fd8b2f4734efbe7c37a313a
Bruce Zhang <zttt183525594@gmail.com>
2016-05-24 18:32:54 +0800

    bug fix

--- src/plugins/linkhandler.py
+++ src/plugins/linkhandler.py
@@ -119,7 +119,7 @@ def _get_url_title(con):
         lines.append(line)
         if str(line).find('</title>') != -1:
             con.close()
-            return BeautifulSoup(''.join(lines), 'html5lib').title.text
+            return BeautifulSoup(b''.join(lines), 'html5lib').title.text
 
 
 def _formatted_size(size):
