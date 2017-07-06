d84af676a0670dda0a0e0fdae4f42458825d099f
Bruce Zhang <zttt183525594@gmail.com>
2017-07-06 15:26:28 +0800

    plugin autoimport

--- pyircbot/__main__.py
+++ pyircbot/__main__.py
@@ -19,25 +19,23 @@ import signal
 import importlib
 from os.path import splitext
 
+logger = logging.getLogger('ircbot')
+
 def load_plugins(plugin_dir):
     plugins = list(map(lambda file: splitext(file)[0] ,filter(lambda file: file.endswith('.py') and not file.startswith('__init__'), os.listdir(plugin_dir))))
     loaded_plugins = []
     for plugin in plugins:
         try:
-            mod = importlib.import_module('plugins.%s' % plugin)
-            exec("%s = mod.%s" % (plugin, plugin))
+            exec("from plugins.%s import %s" % (plugin, plugin))
             loaded_plugins.append(plugin)
-        except ImportError:
+        except ImportError as e:
+            print(e);
             logger.warning("Cannot load plugin `%s`, ignoring it." % plugin)
     return loaded_plugins
 
 PREFIX = '$'
 PLUGINDIR = './plugins/'
 pluginss = load_plugins(PLUGINDIR)
-
-from plugins.echo import echo
-from plugins.linkhandler import linkhandler
-from plugins.ip import ip
 
 class MyBot(irc.bot.SingleServerIRCBot):
 
