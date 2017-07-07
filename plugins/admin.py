2874d5ca578755c8bbe5604bee356729e2054e26
Bruce Zhang <zttt183525594@gmail.com>
2017-07-07 10:08:39 +0800

    fix admin plugin

--- plugins/admin.py
+++ plugins/admin.py
@@ -27,7 +27,7 @@ def admin(args, nick, channel, c, e):
                 module = importlib.import_module("plugins.%s" % arg1)
                 importlib.reload(module)
                 exec("global %s" % arg1)
-                exec("%s=getattr(module, plugin)" % arg1)
+                exec("%s=getattr(module, arg1)" % arg1)
                 return "%s: Plugin `%s` reloaded." % (nick, arg1)
             except:
                 return "%s: Plugin `%s` not found." % (nick, arg1)
