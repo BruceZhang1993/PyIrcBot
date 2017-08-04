45d9638f4c4cb637f7844309a61903211db9f51f
Bruce Zhang <zttt183525594@gmail.com>
2017-08-04 23:40:18 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,14 @@ setup(
     author='BruceZhang1993',
     author_email='zttt183525594@gmail.com',
     description='Yet another python based IRC bot framework.',
-    scripts=['ircbot']
+    scripts=['ircbot'],
+    install_requires=[
+        'irc',
+        'termcolor',
+        'requests',
+        'Pillow',
+        'BeautifulSoup4',
+        'html5lib'
+    ],
+    keywords=['irc', 'bot']
 )
