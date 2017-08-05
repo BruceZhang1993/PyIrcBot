dc1f9f1aa571a8305ec66122fb3bae4b5f4d87c2
Bruce Zhang <zttt183525594@gmail.com>
2017-08-05 22:36:30 +0800

    Revert "update linkhandler 3"
    This reverts commit 6776dd03401202073cb6c2ec404247a66742c2b1.

--- plugins/linkhandler.py
+++ plugins/linkhandler.py
@@ -142,9 +142,15 @@ def _get_url_info(con):
 
 def _get_wiki(url):
     try:
-        con = requests.get(url, allow_redirects=True)
+        con = requests.get(url, stream=True, allow_redirects=True)
         con.raise_for_status()
-        soup = BeautifulSoup(con.content, 'html5lib')
+        lines = []
+        for line in con.iter_lines():
+            lines.append(line)
+            if str(line).find('<div id="mw-navigation">') != -1:
+                con.close()
+                break
+        soup = BeautifulSoup(b''.join(lines), 'html5lib')
         first_para = soup.soup.find(class_='mw-parser-output').find('p').text().strip()
         textarr = first_para.split('.', num=1)
         return textarr[0]
